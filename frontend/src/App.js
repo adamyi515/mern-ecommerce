be28052cca121801958b83746611b54725e7d56f
0mni-gears515 <adamyi515@gmail.com>
2022-11-16 15:53:50 -0600

    Add react router to the header component. Add react router to the product-card component. Finish the layout for the product screen page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
 // Components
 import Header from "./components/header.component";
 import Footer from "./components/footer.component";
+
+// Pages/Screens
 import HomeScreen from "./screens/home-screen.component";
+import ProductScreen from './screens/product-screen.component';
 
 // Bootstrap
 import { Container } from 'react-bootstrap';
 
+// React router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
         <main className="py-3">
           <Container>
-              <HomeScreen />
+              <Routes>
+                <Route path='/' element={<HomeScreen />} />
+                <Route path='/product/:id' element={<ProductScreen />} />
+              </Routes>
           </Container>
         </main>
       <Footer />
-    </div>
+    </BrowserRouter>
   ); 
 }
 
--- frontend/src/components/product-card.component.jsx
+++ frontend/src/components/product-card.component.jsx
@@ -1,21 +1,24 @@
 import { Card } from 'react-bootstrap';
 
+// Router
+import { Link } from 'react-router-dom';
+
 // Components
 import Rating from '../components/rating.component';
 
 const ProductCard = ({ product }) => {
     return(
         <Card className='my-3 p-3 rounded'>
-            <a href={`product/${product._id}`}>
+            <Link to={`product/${product._id}`}>
                 <Card.Img src={product.image} variant='top' />
-            </a>
+            </Link>
 
             <Card.Body>
-                <a href={`product/${product._id}`}>
+                <Link to={`product/${product._id}`}>
                     <Card.Title as='div'>
                         <strong>{ product.name }</strong>
                     </Card.Title>
-                </a>
+                </Link>
             </Card.Body>
 
 
